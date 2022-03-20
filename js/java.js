69a510012e366da621989822d542847396a4870e
GGBEKA <88777197+GGBEKA@users.noreply.github.com>
2022-03-20 16:17:14 +0600

    0.0.1
    first ver.

---
+++ js/java.js
@@ -0,0 +1,39 @@
+function register(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "login.html"; }, 900);
+}
+
+function cars(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "cars.html"; }, 900);
+}
+
+function main(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "main.html"; }, 900);
+}
+
+function info(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "info.html"; }, 900);
+}
+
+function car1(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "info.html#car1"; }, 900);
+}
+
+function car2(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "info.html#car2"; }, 900);
+}
+
+function car3(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "info.html#car3"; }, 900);
+}
+
+function car4(){
+addEventListener('click',function(){document.getElementById('sndmotor').play()})
+setTimeout(() => { document.location.href = "info.html#car4"; }, 900);
+}
